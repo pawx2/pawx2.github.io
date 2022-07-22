a36e6b3411fb38ef7bf7185a86f1dd25f2decd60
pawx2 <2991908515@qq.com>
2022-07-22 20:14:11 +0800

    Site updated: 2022-07-22 20:14:10

--- onlineHTML/scripts/data.js
+++ onlineHTML/scripts/data.js
@@ -8,6 +8,6 @@ let app = new Cow({
         msg:'<h1>Hello Cow!😊</h1>',
         fontSize:15,
         itemTitle:'OnlineHTML-ByCow',
-        itemIntro:'自实现的名为Cow的MVVM框架<br>&基于Cow的在线HTML编辑器<br>by pawx2'
+        itemIntro:'自实现的MVVM框架Cow<br>&基于Cow的在线HTML编辑器<br>by pawx2'
     }
 })
